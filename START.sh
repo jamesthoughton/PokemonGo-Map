08011b1ca204c99bf44ef28d2defe554a7f676be
jamesthoughton <jamesthoughton@gmail.com>
2016-07-21 01:53:19 -0400

    Restart script

--- START.sh
+++ START.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
-echo "python2 example.py -a ptc -u $POKE_USER -p **** -l "$POKE_LOC" -st $POKE_RAD --port $POKE_PORT"
-python2 example.py -a ptc -u $POKE_USER -p $POKE_PASS -l "$POKE_LOC" -st $POKE_RAD --port $POKE_PORT
+while true; do
+    . exports/cameron/0.sh
+    echo "python2 example.py -a ptc -u $POKE_USER -p **** -l "$POKE_LOC" -st $POKE_RAD --port $POKE_PORT";
+    python2 example.py -a ptc -u $POKE_USER -p $POKE_PASS -l "$POKE_LOC" -st $POKE_RAD --port $POKE_PORT &
+
+    . exports/cameron/1.sh
+    echo "python2 example.py -a ptc -u $POKE_USER -p **** -l "$POKE_LOC" -st $POKE_RAD --port $POKE_PORT";
+    python2 example.py -a ptc -u $POKE_USER -p $POKE_PASS -l "$POKE_LOC" -st $POKE_RAD --port $POKE_PORT &
+
+    . exports/dan/0.sh
+    echo "python2 example.py -a ptc -u $POKE_USER -p **** -l "$POKE_LOC" -st $POKE_RAD --port $POKE_PORT";
+    python2 example.py -a ptc -u $POKE_USER -p $POKE_PASS -l "$POKE_LOC" -st $POKE_RAD --port $POKE_PORT &
+
+    . exports/dan/1.sh
+    echo "python2 example.py -a ptc -u $POKE_USER -p **** -l "$POKE_LOC" -st $POKE_RAD --port $POKE_PORT";
+    python2 example.py -a ptc -u $POKE_USER -p $POKE_PASS -l "$POKE_LOC" -st $POKE_RAD --port $POKE_PORT &
+
+    # j=$!;
+    sleep 1000;
+    echo "Restarting";
+    # echo "kill -2 $j";
+    # kill -2 $j;
+    pkill python2;
+done;
